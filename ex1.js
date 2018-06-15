c0d433f9e0840314f048b66e8748522531dfce57
Beatriz Mendes <bmendesgs@gmail.com>
2018-06-15 11:43:06 -0300

    Exercitar javascript

---
+++ ex1.js
@@ -0,0 +1,12 @@
+let idade = prompt ("Digite sua idade.");
+if (idade >= 22 && idade <= 55) {
+    alert("")
+}
+let emprestimo = prompt ("Quanto quer emprestar?");
+if (emprestimo >= 1000) {
+    alert("")
+}
+let renda = prompt ("Digite sua renda mensal.");
+if (renda ) {
+    alert("")
+}
---
+++ ex5.js
@@ -0,0 +1,5 @@
+let usuario = prompt ("Qual usuário?");
+let senha = prompt ("Digite sua senha.");
+let nascimento = prompt ("Digite sua data de nascimento.");
+let premium = confirm ( )
+
